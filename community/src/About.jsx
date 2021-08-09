be0814f0f48d3aac4d7edf8bc3a2e62a1921f435
100Gyeon <bjy9966@naver.com>
2021-08-09 20:40:16 +0900

    소개 페이지

---
+++ community/src/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const divStyle = {
+  margin: 50
+};
+
+const About = () => {
+  return (
+    <div style={divStyle}>
+      <h1><strong>소개</strong></h1>
+      <br></br>
+      <p>20180763 백지연</p>
+      <p>웹프로그래밍설계및실습 커뮤니티 서비스 과제입니다.</p>
+    </div>
+  );
+};
+
+export default About;
