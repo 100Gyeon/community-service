c1fe31b081764da6581a18b1381a21172b87be82
100Gyeon <bjy9966@naver.com>
2021-12-27 04:23:23 +0900

    소개 페이지

--- community/src/About.jsx
+++ community/src/About.jsx
@@ -7,10 +7,25 @@ const divStyle = {
 const About = () => {
   return (
     <div style={divStyle}>
-      <h1><strong>소개</strong></h1>
+      <h1>
+        <strong>소개</strong>
+      </h1>
       <br></br>
       <p>20180763 백지연</p>
       <p>웹프로그래밍설계및실습 커뮤니티 서비스 과제입니다.</p>
+      <div>1. 기본 기능 구성</div>
+      <ul>
+        <li>사용자 등록, 로그인, 로그아웃 (/)</li>
+        <li>소개 페이지 (/about)</li>
+        <li>게시판 (/board)</li>
+        <li>갤러리 (/gallery)</li>
+      </ul>
+      <div>2. 개발 환경</div>
+      <ul>
+        <li>Front-end : React</li>
+        <li>Back-end : Node.js</li>
+        <li>Database : MongoDB</li>
+      </ul>
     </div>
   );
 };
